983da61871cd2d0bbc737b2950b0fc0db0aee26e
Christoffer, Marschner <christoffer.marschner@sew-eurodrive.de>
2024-12-20 13:47:35 +0100

    funktions skelett in agv, sowie benötigte getter und setter hinzugefügt

--- AGV-BSS/cAGV.cs
+++ AGV-BSS/cAGV.cs
@@ -17,4 +17,59 @@ class cAGV
     this.status = agvStatus_n.verfuegbar;
   }
 
+  public int getX()
+  {
+    return this.x;
+  }
+
+  public int getY()
+  {
+    return this.y;
+  }
+
+  public agvStatus_n getStatus()
+  {
+    return this.status;
+  }
+
+  public void setStatus(agvStatus_n neuerStatus)
+  {
+    this.status = neuerStatus;
+  }
+
+  public int removeAuftrag()
+  {
+    // TODO auftrag entfernen - return erfolgsstatus 
+  }
+
+  public int auftragZuweisen(acKartenlement[] karte, cAuftrag auftrag)
+  {
+    // TODO auftrag speichen - route berechnen - return erfolgsstatus
+  }
+
+  private int besteRouteBerechnen(acKartenlement[] karte, cAuftrag auftrag)
+  {
+    // TODO route aus dem auftrag und kartenelement mit dem a* ähnlichen algorithmus berechnen - return erfolgsstatus
+  }
+
+  private void fahrtInitialisieren()
+  {
+    // TODO status auf fahren setzen - initialisierung, bevor die route abgefahren wird
+  }
+
+  public void fahrtAbbrechen()
+  {
+    // TODO agv anhalten
+  }
+
+  public void fahrtZurLadestation()
+  {
+    // TODO route zur ladestation berechen - fahrt zur ladestation einleiten
+  }
+
+  public void navigieren()
+  {
+    // TODO nächsten punkt in der route anfahren
+  }
+
 }
