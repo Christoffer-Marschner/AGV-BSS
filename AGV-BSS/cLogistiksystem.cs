b3fe2b5551f2ebbc7f2da5ecb756fec0f9bfb90f
Christoffer, Marschner <christoffer.marschner@sew-eurodrive.de>
2024-12-20 13:08:56 +0100

    auftrag und logistiksystem skelett hinuzgefügt

---
+++ AGV-BSS/cLogistiksystem.cs
@@ -0,0 +1,27 @@
+using System;
+
+class cLogistiksystem 
+{
+  private cAuftrag[] auftragsListe = new cAuftrag[];
+
+  public cAuftrag getAuftrag()
+  {
+    // loop durch alle aufträge und gib den mit der höchsten prio zurück
+  }
+  
+  public void addAuftrag(cAuftrag neuerAuftrag)
+  {
+    // auftrag an die liste anhängen
+  }
+  
+  public void deleteAuftrag(int auftragsId)
+  {
+    // auftrag an einer id entfernen
+  }
+  
+  public void setAuftragStatus(int auftragsId, int neuerStatus)
+  {
+    // loop durch alle aufträge bis die id gefunden wird, dann wird der status gesetzt
+  }
+  
+}
